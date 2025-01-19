32d84c94e081cec55c9fa8f4fda6afbd8f540786
Atl4sDev <mr.shkarnikov@gmail.com>
2025-01-19 15:21:34 +0200

    Initial commit

---
+++ src/main/java/edu/catscosmicmarket/CatsCosmicMarketApplication.java
@@ -0,0 +1,13 @@
+package edu.catscosmicmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatsCosmicMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatsCosmicMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/catscosmicmarket/CatsCosmicMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.catscosmicmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatsCosmicMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
